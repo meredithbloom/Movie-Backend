21d55898c3f147c4fb075804dd776feba5060115
Heather Mielke <heatherlmielke@gmail.com>
2022-03-02 10:17:29 -0600

    added connection to atlas

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const app = express()
 const mongoose = require('mongoose')
 const movieControllers = require('./controllers/movies.js')
 const cors = require('cors')
+const db = mongoose.connection;
+require('dotenv').config()
 
+const PORT = process.env.PORT || 3003;
+
+const PROJECT3_DB = process.env.PROJECT3_DB
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?...'))
+db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('disconnected', () => console.log('mongo disconnected......'))
 
 app.use(express.json())
 app.use(cors())
@@ -14,6 +23,3 @@ app.listen(3000, () => {
 })
 
 mongoose.connect('mongodb://localhost:27017/movies')
-mongoose.connection.once('open', () => {
-  console.log('connected to mongoose...movies....');
-})
