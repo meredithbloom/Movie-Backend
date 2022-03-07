9510ad49f6086781e9a8090a312e9c5038b30c42
Heather Mielke <heatherlmielke@gmail.com>
2022-03-07 12:42:03 -0600

    code

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema ({
   name: {type:String, required:true},
   email: {type:String, required:true, unique:true},
   username: {type: String, required:true, unique:true},
-  password: {type:String, required:true}, 
+  password: {type:String, required:true},
   profilePic: {type: String, default: './userimage.png'}
 })
 
